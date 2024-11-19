fb14938a9566d82133bc73af765ce015bc0db83e
g2ro <kilho603@gmail.com>
2024-11-19 16:17:56 +0900

    init commit

---
+++ practice01/src/main/java/prob05/Sol05.java
@@ -0,0 +1,19 @@
+package prob05;
+
+public class Sol05 {
+	public static void main(String[] args) {
+
+		for(int i = 1; i<= 100; i++) {
+			
+			System.out.print(i);
+			// System.out.print( i + '\n');
+			
+			String number = String.valueOf(i);
+			
+			System.out.print(" ");
+			System.out.print(number.length());
+			System.out.print("\n");
+			/*문자열 반환 => 3,6,9비교*/
+		}
+	}
+}
