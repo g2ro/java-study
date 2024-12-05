a1e48fd30827e0ab2d2df05cf3f6b0a7c749028f
g2ro <kilho603@gmail.com>
2024-12-06 08:44:38 +0900

    solving practice04_prob

--- practice04/src/main/java/prob06/Money.java
+++ practice04/src/main/java/prob06/Money.java
@@ -4,4 +4,37 @@ public class Money {
 	private int amount;
 	
 	/* 코드 작성 */
+	public Money(int amount) {
+		this.amount = amount;
+	}
+	
+	public Object add(Money money) {
+		int result = amount + money.amount;
+		return new Money(result);
+	}
+	
+	public Object minus(Money money) {
+		int result = amount - money.amount;
+		return new Money(result);
+	}
+	
+	public Object multiply(Money money) {
+		int result = amount * money.amount;
+		return new Money(result);
+	}
+	
+	public Object divide(Money money) {
+		int result = amount / money.amount;
+		return new Money(result);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Money) {
+			Money other = (Money)obj;
+			return amount == other.amount;
+		} else {
+			return false;
+		}			
+	}	
 }
