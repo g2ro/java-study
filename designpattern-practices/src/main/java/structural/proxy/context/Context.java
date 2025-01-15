84d83c72e43313265a14a64bfb19d515f79a8f0a
g2ro <kilho603@gmail.com>
2025-01-15 14:15:35 +0900

    proxy pattern

---
+++ designpattern-practices/src/main/java/structural/proxy/context/Context.java
@@ -0,0 +1,18 @@
+package structural.proxy.context;
+
+import structural.proxy.i.Subject;
+
+class Context {
+	private Subject realSubject;
+	
+	Context() {
+		this(() -> {System.out.println("Subject.doAction in Context done");});
+	}
+	private Context(Subject subject) {
+		realSubject = subject;
+	}
+	Subject getSubject() {
+		
+		return realSubject;
+	}
+}
